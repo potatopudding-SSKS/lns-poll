91ae2dcdd9edeb9d5282002e2c62dd25216ce55a
Saisankalp <kssaisankalp.davey@research.iiit.ac.in>
2025-12-01 15:32:30 +0530

    temp2

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ from uuid import uuid4
 from streamlit_sortables import sort_items
 
 # Configuration variables
-N_RANDOM_CLIPS = 4  # Number of random clips to show all participants
-M_LANGUAGE_CLIPS = 2  # Number of language-specific clips to show
+N_RANDOM_CLIPS = 1  # Number of random clips to show all participants
+M_LANGUAGE_CLIPS = 0  # Number of language-specific clips to show
 
 # Initialize Firebase service with caching
 @st.cache_resource
